8e7d13381a75d752dc60e085b1d445069348c953
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2019-08-08 15:00:50 +0200

    ny struktur uden modul i roden core-modul kalder de andre moduler

---
+++ src/main/java/eu/nitrogensensor/core/Main.java
@@ -0,0 +1,12 @@
+package eu.nitrogensensor.core;
+
+public class Main
+{
+
+  public static void main(String[] args) {
+    System.out.println("hej fra core");
+    eu.nitrogensensor.fmis.Main.main(args);
+    eu.nitrogensensor.datamodel.Main.main(args);
+
+  }
+}
---
+++ src/main/java/eu/nitrogensensor/fmis/Main.java
@@ -0,0 +1,8 @@
+package eu.nitrogensensor.fmis;
+
+public class Main
+{
+  public static void main(String[] args) {
+    System.out.println("hej fra fmis");
+  }
+}
