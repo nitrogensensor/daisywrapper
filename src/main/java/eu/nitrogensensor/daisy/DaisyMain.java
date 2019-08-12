c7ea17bcc5f89b5804054686a20d4efec5dc0fbb
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2019-08-12 14:54:42 +0200

    mere modulstruktur

---
+++ src/main/java/eu/nitrogensensor/daisy/DaisyMain.java
@@ -0,0 +1,11 @@
+package eu.nitrogensensor.daisy;
+
+
+public class DaisyMain
+{
+
+  public static void main(String[] args) {
+    System.out.println("hej fra DaisyMain");
+
+  }
+}
--- src/main/java/eu/nitrogensensor/fmis/Main.java
+++
@@ -1,8 +0,0 @@
-package eu.nitrogensensor.fmis;
-
-public class Main
-{
-  public static void main(String[] args) {
-    System.out.println("hej fra fmis");
-  }
-}
