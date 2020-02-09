cc105cb6f66caf3765392a2bcd10c2b48b196063
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2020-02-09 21:34:05 +0100

    Videre arbejde med at bearbejde output og lave udtræk

--- src/main/java/eu/nitrogensensor/daisy/DaisyMain.java
+++ src/main/java/eu/nitrogensensor/daisy/DaisyMain.java
@@ -2,8 +2,12 @@ package eu.nitrogensensor.daisy;
 
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -11,6 +15,7 @@ import java.util.concurrent.atomic.AtomicReference;
 
 public class DaisyMain
 {
+  private static final boolean FILPRÆFIX_PÅ_KOLONNER = true;
 
   public static void main(String[] args) throws IOException, InterruptedException {
     System.out.println("hej fra DaisyMain");
@@ -23,38 +28,87 @@ public class DaisyMain
     //kørsel0.erstat("stop 2018 8 20", "stop 2015 8 20"); // for hurtigere kørsel
     kørsel0.erstat("(path *)", "(path \"/opt/daisy/sample\" \"/opt/daisy/lib\" \".\" \"./common\")");
 
+/*
+    kørsel.erstat("(defprogram taastrup batch*)", "(defprogram taastrup batch (run (Mark21_hnlw)))");
+    kørsel.erstat("(defprogram taastrup batch*)", "(defprogram taastrup batch (run (Mark21_lnlw)))");
+    kørsel.erstat("(defprogram taastrup batch*)", "(defprogram taastrup batch (run (Mark21_lnhw)))");
+    kørsel.erstat("(defprogram taastrup batch*)", "(defprogram taastrup batch (run (Mark21_hnhw)))");
+
     String[] programmer = {
             "(run (Mark21_hnlw))",
             "(run (Mark21_lnlw))",
             "(run (Mark21_lnhw))",
             "(run (Mark21_hnhw))",
     };
-/*
-    kørsel.erstat("(defprogram taastrup batch*)", "(defprogram taastrup batch (run (Mark21_hnlw)))");
-    kørsel.erstat("(defprogram taastrup batch*)", "(defprogram taastrup batch (run (Mark21_lnlw)))");
-    kørsel.erstat("(defprogram taastrup batch*)", "(defprogram taastrup batch (run (Mark21_lnhw)))");
-    kørsel.erstat("(defprogram taastrup batch*)", "(defprogram taastrup batch (run (Mark21_hnhw)))");
 */
+    String[] programmer = {
+            "(run Mark21 (column (\"High_N_Low_W\")))",
+//            "(run Mark21 (column (\"Low_N_Low_W\")))",
+//            "(run Mark21 (column (\"Low_N_High_W\")))",
+//            "(run Mark21 (column (\"High_N_High_W\")))",
+    };
 
+    kørsel0.outputEkstrakt.add(new Koersel.OutputEkstrakt("crop-leaf-stem-AI.csv", "crop.csv (year, month, mday, LAI), crop_prod.csv (year, month, mday, Crop AI, Leaf AI, Stem AI)"));
+    //kørsel0.outputfilnavne = new String[]{"crop.csv", "crop_prod.csv" };
     long tid = System.currentTimeMillis();
 
     ExecutorService executorService = Executors.newWorkStealingPool();
     AtomicReference<IOException> fejl = new AtomicReference<>(); // Hvis der opstår en exception skal den kastes videre
+    int kørselsNr = 0;
     for (String program : programmer) {
+      kørselsNr++;
+      final int kørselsNr_ = kørselsNr;
+      Runnable runnable = () -> {
+        try {
+          if (fejl.get() != null) return;
+          Koersel kørsel = kørsel0.kopi();
+          kørsel.erstat("(run taastrup)", program);
+          Path tmpMappe = Files.createTempDirectory("ns-daisy");
+          tmpMappe = Paths.get("/tmp/p5/k"+kørselsNr_); // Anden mappe i udviklingsøjemed
+          //kørsel.klargørTilMappe(tmpMappe);
+          //daisyInvoke.invokeDaisy(tmpMappe, scriptFil);
 
-      Runnable runnable = new Runnable() {
-        @Override
-        public void run() {
-          if (fejl.get()!=null) return;
-          Koersel k = new Koersel(kørsel0).erstat("(run taastrup)", program);
-          Path tmpMappe = Paths.get("/tmp/p/work_"+program); // Files.createTempDirectory("work");
-          try {
-            k.klargørTilMappe(tmpMappe);
-            daisyInvoke.invokeDaisy(tmpMappe, scriptFil );//daisy/src/test/resources/Taastrup 2019/dtu_model", "Setup_DTU_Taastrup.dai");
-          } catch (IOException e) {
-            e.printStackTrace();
-            fejl.set(e);
+
+          HashSet<String> outputfilnavne = new HashSet<>();
+          for (Koersel.OutputEkstrakt outputEkstrakt : kørsel.outputEkstrakt) {
+            outputfilnavne.addAll(outputEkstrakt.filKolonnerMap.keySet());
           }
+          for (String filnavn : outputfilnavne) {
+            Koersel.Ouputfilindhold output = getOuputfilindhold(tmpMappe, filnavn);
+            kørsel.output.put(filnavn, output);
+          }
+
+          for (Koersel.OutputEkstrakt outputEkstrakt : kørsel.outputEkstrakt) {
+
+            ArrayList<String> kolonner = new ArrayList<>();
+            ArrayList<String> enheder = new ArrayList<>();
+
+            for (String filnavn : outputEkstrakt.filKolonnerMap.keySet()) {
+              Koersel.Ouputfilindhold output = kørsel.output.get(filnavn);
+              for (String kol : outputEkstrakt.filKolonnerMap.get(filnavn)) {
+                if (FILPRÆFIX_PÅ_KOLONNER) kolonner.add(filnavn+":"+kol);
+                else kolonner.add(kol);
+
+                int idx = 0;
+                while (idx<output.kolonnenavne.length && output.kolonnenavne[idx].equals(kol)) idx++;
+                if (idx==output.kolonnenavne.length) throw new IllegalArgumentException("Kolonne '"+kol+"' fandtes ikke i "+output);
+
+
+                enheder.add( output.enheder[idx] );
+              }
+
+
+            }
+
+
+          }
+
+
+          System.out.println(kørsel);
+
+        } catch (IOException e) {
+          e.printStackTrace();
+          fejl.set(e);
         }
       };
       //runnable.run(); // serielt
@@ -66,11 +120,37 @@ public class DaisyMain
 
 
     System.out.printf("Det tog %.1f sek", (System.currentTimeMillis()-tid)/1000.0);
+  }
 
+  static Koersel.Ouputfilindhold getOuputfilindhold(Path tmpMappe, String filnavn) throws IOException {
+    Koersel.Ouputfilindhold output = new Koersel.Ouputfilindhold();
+    output.filnavn = filnavn;
+    String csv = new String(Files.readAllBytes(tmpMappe.resolve(filnavn)));
+    String[] csvsplit = csv.split("--------------------");
+    output.header = csvsplit[0].trim();
+    String[] linjer = csvsplit[1].trim().split("\n");
+    output.kolonnenavne = linjer[0].split("\t");
+    output.enheder = linjer[1].split("\t");
+
+    if (output.kolonnenavne.length < output.enheder.length) { // crop.csv har 24 kolonner, men 21 enheder (de sidste 3 kolonner er uden enhed), derfor < og ikke !=
+      throw new IOException(filnavn + " har " +output.kolonnenavne.length +" kolonner, men "+output.enheder.length+
+              " enheder\nkol="+ Arrays.toString(output.kolonnenavne) +"\nenh="+Arrays.toString(output.enheder));
+    }
+    output.data = new ArrayList<>(linjer.length);
+    for (int n=2; n<linjer.length; n++) {
+      String[] linje = linjer[n].split("\t");
+      if (output.kolonnenavne.length < linje.length || linje.length < output.enheder.length) { // data altid mellem
+        throw new IOException(filnavn + " linje " + n +  " har " +linje.length +" kolonner, men "+
+                output.kolonnenavne.length + " kolonnenavne og "+
+                output.enheder.length +" enheder\nlin="+ Arrays.toString(linje) +" enheder\nkol="+ Arrays.toString(output.kolonnenavne) +"\nenh="+Arrays.toString(output.enheder));
+      }
+      output.data.add(linje);
+    }
+    // Fyld op med tomme enheder
+    return output;
   }
 
   public void invokeDaisy(String mappe, String inputFil) throws InterruptedException, IOException {
 
   }
-
 }
--- src/main/java/eu/nitrogensensor/daisy/Koersel.java
+++ src/main/java/eu/nitrogensensor/daisy/Koersel.java
@@ -3,12 +3,103 @@ package eu.nitrogensensor.daisy;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class Koersel {
+public class Koersel implements Cloneable {
     public final Path orgMappe;
     public final String scriptFil;
-    private final ArrayList<Erstatning> erstatninger = new ArrayList<>();
+    private ArrayList<Erstatning> erstatninger = new ArrayList<>();
+
+    public static class Ouputfilindhold {
+        public String filnavn;
+        public String header;
+        public String[] kolonnenavne;
+        public String[] enheder;
+        public ArrayList<String[]> data;
+
+        @Override
+        public String toString() {
+            return "Ouputfilindhold{" +
+                    "'" + filnavn + '\'' +
+                    ", " + kolonnenavne.length +" kolonner"+
+                    ", " + (data==null?0:data.size())+" rækker" +
+                    '}';
+        }
+    }
+    public final Map<String, Ouputfilindhold> output = new LinkedHashMap<String, Ouputfilindhold>();
+
+    public static void main(String[] args) {
+        new OutputEkstrakt("xx", "crop.csv (year, month, mday, LAI), crop_prod.csv (year, month, mday, Crop AI, Leaf AI, Stem AI)");
+        new OutputEkstrakt("xx", "crop.csv (*)");
+        new OutputEkstrakt("xx", "crop.csv");
+    }
+
+    public ArrayList<OutputEkstrakt> outputEkstrakt = new ArrayList<>();
+    public static class OutputEkstrakt {
+        public final LinkedHashMap<String, ArrayList<String>> filKolonnerMap = new LinkedHashMap<String, ArrayList<String>>();
+        public final Ouputfilindhold output = new Ouputfilindhold();
+
+        /**
+         *
+         * @param skrivTilFilnavn Hvilken fil ekstraktet skal skrives til
+         * @param indhold Hvilket indhold fra hvilke filer der skal trækkes ud. F.eks. giver "crop.csv (year    month   mday LAI), crop_prod.csv (year    month   mday, Crop AI Leaf AI Stem AI)"
+         *                kolonnerne (year    month   mday LAI) fra crop.csv og (year    month   mday, Crop AI Leaf AI Stem AI) fra crop_prod.csv.
+         *                "crop.csv (*)" eller blot "crop.csv" giver hele indholdet af en fil
+         */
+        public OutputEkstrakt(String skrivTilFilnavn, String indhold) {
+            output.filnavn = skrivTilFilnavn;
+
+            Matcher filnavnMatcher = Pattern.compile("[a-zA-Z_. ]+(?![^(]*\\))").matcher(indhold);
+
+
+            String forrigeFil = null;
+            int forrigeMatchSlutpos = 0;
+            while (filnavnMatcher.find()) {
+                if (forrigeFil!=null) {
+                    String forrigeKol = indhold.substring(forrigeMatchSlutpos, filnavnMatcher.start());
+                    filKolonnerMap.put(forrigeFil, findKolonner(forrigeKol));
+                }
+
+                forrigeFil = filnavnMatcher.group().trim();
+                forrigeMatchSlutpos = filnavnMatcher.end();
+            }
+            if (forrigeFil!=null) {
+                String forrigeKol = indhold.substring(forrigeMatchSlutpos);
+                filKolonnerMap.put(forrigeFil, findKolonner(forrigeKol));
+            }
+
+            System.out.println(indhold);
+            System.out.println(this);
+        }
+
+        private ArrayList<String> findKolonner(String sb) {
+            String trimTegn = "(*\t\n ),";
+            // Trim parenteser etc i enderne væk
+            //System.out.println(sb);
+            int start=0;
+            int slut=sb.length()-1;
+            while (start<=slut && trimTegn.indexOf(sb.charAt(start))!=-1) start++;
+            while (start<=slut && trimTegn.indexOf(sb.charAt(slut))!=-1) slut--;
+
+            String s = sb.substring(start, slut+1);
+            //System.out.println(s);
+            ArrayList<String> kolonner = new ArrayList<>();
+            for (String kol : s.split("[,\t]+")) kolonner.add(kol.trim());
+            return kolonner;
+        }
+
+
+        @Override
+        public String toString() {
+            return "OutoutEkstrakt{" +
+                    ", filKolonnerMap=" + filKolonnerMap +
+                    '}';
+        }
+    }
+
+
 
     public Koersel(Path orgMappe, String scriptFil) {
         this.orgMappe = orgMappe;
@@ -16,9 +107,28 @@ public class Koersel {
     }
 
     /** Opretter en kopi af en kørsel og kopi af dets erstatninger */
-    public Koersel(Koersel kørsel) {
-        this(kørsel.orgMappe, kørsel.scriptFil);
-        erstatninger.addAll(kørsel.erstatninger);
+    public Koersel kopi() {
+        if (!output.isEmpty()) throw new IllegalStateException("Du bør ikke bruge en kørsel der allerede har output som skabelon for en anden kørsel");
+        try {
+            Koersel kopi = (Koersel) this.clone();
+            kopi.erstatninger = new ArrayList<>();
+            kopi.erstatninger.addAll(this.erstatninger);
+            return kopi;
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Koersel{" +
+                "orgMappe=" + orgMappe +
+                ", scriptFil='" + scriptFil + '\'' +
+                ", erstatninger=" + erstatninger +
+                ", outputfilnavne=" + outputEkstrakt +
+                ", output=" + output +
+                '}';
     }
 
     public void klargørTilMappe(Path destMappe) throws IOException {
