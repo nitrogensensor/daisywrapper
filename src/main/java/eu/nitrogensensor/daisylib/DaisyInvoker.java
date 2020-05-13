80532d8d4cf7db617b7ae1af77c6671379040731
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2020-05-13 15:37:47 +0200

    Christians videokomponent for de andre JSON'er

--- src/main/java/eu/nitrogensensor/daisylib/DaisyInvoker.java
+++ src/main/java/eu/nitrogensensor/daisylib/DaisyInvoker.java
@@ -15,8 +15,8 @@ class DaisyInvoker {
         prop.load(input);
 
         int exitValue;
-        System.out.println("RUN DAISY! "+mappe+" "+inputFil);
-        System.out.println(new File(prop.getProperty("daisy.executable.path")).getAbsolutePath());
+        //System.out.println("RUN DAISY! "+mappe+" "+inputFil);
+        //System.out.println(new File(prop.getProperty("daisy.executable.path")).getAbsolutePath());
 
         Process process = new ProcessBuilder(new File(prop.getProperty("daisy.executable.path")).getAbsolutePath(), inputFil)
                 .redirectInput(ProcessBuilder.Redirect.INHERIT)
