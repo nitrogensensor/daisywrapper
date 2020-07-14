efa3dd6327228308544d96dcbd13a57c48d722bf
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2020-07-14 16:52:32 +0200

    Nye parametre --replace, --repeat-replace, --replicate-replace

--- src/main/java/eu/nitrogensensor/daisy/DaisyMain.java
+++ src/main/java/eu/nitrogensensor/daisy/DaisyMain.java
@@ -21,12 +21,26 @@ public class DaisyMain implements Callable
   @CommandLine.Parameters(index = "0", description = "server, run, remote eller testkørsel." )
   String kommando;
 
-  @CommandLine.Option(names = {"-d", "--directory"}, description = "Hvor mappen med Daisy-filerne er", defaultValue = ".")
-  String dir;
+  @CommandLine.Option(names = {"-d", "--inputdirectory"}, description = "Mappen med Daisy-filerne, der skal køres", defaultValue = ".")
+  String inputdirectory;
 
-  @CommandLine.Parameters(index = "1..", description = "Daisyfiler")
+  @CommandLine.Parameters(index = "1..", description = "Daisyfil(er), der skal køres i mappen")
   List<String> daisyfiler;
+/*
+  @CommandLine.Option(names = {"-r", "--replace"}, description = "Erstatninger der skal ske i daisyfilen før den køres. Hver erstatning består af et søgeudtryk og en erstatningsstreng adskilt af komma. Eksempler\n" +
+          "-r _sand_,37.1   søger efter '_sand_' og erstatter med '37.1'\n" +
+          "-r '(stop *),(stop 2015 8 20)'  sætter stoptidspunkt for simuleringen ")
+  List<String> replace;
 
+  @CommandLine.Option(names = {"-rt", "--repeat-replace"}, description = "Gentagelse af kørslen med forskellige erstatninger." +
+            "-rt _sand_:_humus_,10:90,20:80,30:70,40:60,50:50  giver 5 kørsler hvor sand stiger fra 10 til 50 og humus falder fra 90 til 50 i skridt af 10")
+  List<String> repeatReplace;
+
+  @CommandLine.Option(names = {"-rp", "--replicate-replace"}, description = "Replikering af kørslen med forskellige erstatninger. Hver består af et søgeudtryk og et antal erstatningsstrenge adskilt af komma." +
+          "Formatet er: søg,erstat1,erstat2,erstat3. Er der flere sæt replikerede erstatninger multipliceres de. Eksempelvis giver nedenstående i alt 25 kørsler:\n" +
+          "-rp _sand_,0,10,20,30,40 -rp _humus_,50,60,70,80,90")
+  List<String> replicateReplace;
+*/
   @CommandLine.Option(names = {"-o", "--outputdirectory"}, description = "Hvor skal resultatet skrives til", defaultValue = ".")
   String outputdirectory;
 
@@ -57,11 +71,54 @@ public class DaisyMain implements Callable
       else if ("remote".equals(kommando)) {
           ArrayList<DaisyModel> daisyModels = new ArrayList<>();
           for (String daisyfil : daisyfiler) {
-              DaisyModel dm = new DaisyModel(dir, daisyfil);
+              DaisyModel dm = new DaisyModel(inputdirectory, daisyfil);
               if (daisyfil.endsWith(".dai")) daisyfil = daisyfil.substring(0, daisyfil.length()-4);
               dm.setId(daisyfil);
+/*
+              for (String rElem : replace) {
+                  String[] søgErstat = rElem.split(",");
+                  if (søgErstat.length != 2) throw new IllegalArgumentException("Fejl i "+replace+" for "+rElem+": Formatet er: søg,erstat  - med komma imellem.");
+                  dm.replace(søgErstat[0],søgErstat[1]);
+              }
+
+ */
               daisyModels.add(dm);
           }
+/*
+          for (String elem : repeatReplace) {
+              ArrayList<DaisyModel> nydaisyModels = new ArrayList<>();
+              String[] søgErstat = elem.split(",");
+              if (søgErstat.length < 3) throw new IllegalArgumentException("Fejl i "+repeatReplace+" for "+elem+": Formatet er: søgA:søgB,erstat1A:erstat1B,erstat2A:erstat2B,erstat3A,erstat3B  - med komma imellem.");
+
+              for (DaisyModel dm0 : daisyModels) {
+                  for (int i=1; i<søgErstat.length; i++) {
+                      DaisyModel dm1 = dm0.clon();
+                      dm1.replace(søgErstat[0], søgErstat[i]);
+                      dm1.setId(dm0.getId()+","+søgErstat[i]);
+                      nydaisyModels.add(dm1);
+                  }
+              }
+              daisyModels = nydaisyModels;
+          }
+
+
+          for (String elem : replicateReplace) {
+              ArrayList<DaisyModel> nydaisyModels = new ArrayList<>();
+              String[] søgErstat = elem.split(",");
+              if (søgErstat.length < 3) throw new IllegalArgumentException("Fejl i "+replicateReplace+" for "+elem+": Formatet er: søg,erstat1,erstat2,erstat3  - med komma imellem.");
+
+              for (DaisyModel dm0 : daisyModels) {
+                  for (int i=1; i<søgErstat.length; i++) {
+                      DaisyModel dm1 = dm0.clon();
+                      dm1.replace(søgErstat[0], søgErstat[i]);
+                      dm1.setId(dm0.getId()+","+søgErstat[i]);
+                      nydaisyModels.add(dm1);
+                  }
+              }
+              daisyModels = nydaisyModels;
+          }
+*/
+
           ResultExtractor re = new ResultExtractor();
           for (String outputfil : outputfiler) re.addFile(outputfil);
 
