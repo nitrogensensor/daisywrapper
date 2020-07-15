2600b90b1c3cd5e7c7c5cb757a39c3ec4ffa04fb
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2020-07-15 09:51:09 +0200

    Jeppes properties-idé fejler hvis der recompileres, fordi JAR-filen slettes og en ny JAR dannes

--- src/main/java/eu/nitrogensensor/daisylib/DaisyInvoker.java
+++ src/main/java/eu/nitrogensensor/daisylib/DaisyInvoker.java
@@ -2,23 +2,35 @@ package eu.nitrogensensor.daisylib;
 
 
 // Kilde: Jeppes arbejde
+
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Path;
-import java.util.Properties;
 
 class DaisyInvoker {
     public void invokeDaisy(Path mappe, String inputFil) throws IOException {
+        /*
+        Jeppes properties-idé fejler hvis der recompileres, fordi JAR-filen slettes og en ny JAR dannes. Jacob 15 juli 2020.
         Properties prop = new Properties();
         InputStream input = DaisyInvoker.class.getClassLoader().getResourceAsStream("daisy.properties");
         prop.load(input);
+         */
+        String daisyPath = System.getenv("DAISY_PATH");
+        if (daisyPath==null) {
+            daisyPath = "/opt/daisy/bin/daisy";
+            if (!new File(daisyPath).exists()) {
+                System.err.println("Ingen Daisy i "+daisyPath+" og DAISY_PATH er ikke sat");
+                daisyPath = "daisy";
+            }
+        }
+
+
 
         int exitValue;
         //System.out.println("RUN DAISY! "+mappe+" "+inputFil);
         //System.out.println(new File(prop.getProperty("daisy.executable.path")).getAbsolutePath());
 
-        Process process = new ProcessBuilder(new File(prop.getProperty("daisy.executable.path")).getAbsolutePath(), inputFil)
+        Process process = new ProcessBuilder(daisyPath, inputFil)
                 .redirectInput(ProcessBuilder.Redirect.INHERIT)
                 .redirectOutput(ProcessBuilder.Redirect.INHERIT)
 //                .redirectError(ProcessBuilder.Redirect.DISCARD)
